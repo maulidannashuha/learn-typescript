2c258761b2f3c05c7a0752d341e578639ebda9db
Maulidan Nashuha <maulidannashuha@gmail.com>
2024-06-26 15:12:26 +0700

    learn tupple

---
+++ tests/tupple.ts
@@ -0,0 +1,7 @@
+describe('tupple', () => {
+    it('should support tupple', () => { 
+        const person: readonly [String, String, Number] = ['Maulidan', 'Nashuha', 29]
+        
+        // person[0] = 'new name'
+    })
+})
